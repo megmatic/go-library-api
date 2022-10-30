accf56d4d75cefb881c8f482c4fabf5f8f8bcbf8
megmatic <helloiammegg@gmail.com>
2022-10-30 16:53:08 -0500

    implement api to checkout a book

--- main.go
+++ main.go
@@ -14,13 +14,37 @@ type book struct {
 }
 
 var books = []book{
-	{ID: "1", Title: "Project Hail Mary", Author: "Andy Weir", Quantity: 15},
-	{ID: "2", Title: "The Fifth Season", Author: "N. K. Jemisin", Quantity: 5},
-	{ID: "3", Title: "The Seven Husbands of Evelyn Hugo", Author: "Taylor Jenkins Reid", Quantity: 5},
+	{ID: "1", Title: "Project Hail Mary", Author: "Andy Weir", Quantity: 2},
+	{ID: "2", Title: "The Fifth Season", Author: "N. K. Jemisin", Quantity: 2},
+	{ID: "3", Title: "The Seven Husbands of Evelyn Hugo", Author: "Taylor Jenkins Reid", Quantity: 2},
 }
 
 func getBooks(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, books)
+}
+
+func checkoutBook(context *gin.Context) {
+	id, ok := context.GetQuery("id")
+
+	if !ok {
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
+		return
+	}
+
+	book, err := findBookById((id))
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return
+	}
+
+	if book.Quantity <= 0 {
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available."})
+		return
+	}
+
+	book.Quantity -= 1
+	context.IndentedJSON(http.StatusOK, book)
 }
 
 func findBookById(id string) (*book, error) {
@@ -57,7 +81,8 @@ func createBook(context *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
-	router.POST("/books", createBook)
 	router.GET("/books/:id", getBookById)
+	router.POST("/books", createBook)
+	router.PATCH("/checkout", checkoutBook)
 	router.Run("localhost:8080")
 }
